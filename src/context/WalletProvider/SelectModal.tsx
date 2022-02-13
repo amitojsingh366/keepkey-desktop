938ec99143a20a314c4e866c8bac2ba28edbd096
highlander <bithighlander@gmail.com>
2022-02-13 15:51:46 -0700

    debug

--- src/context/WalletProvider/SelectModal.tsx
+++ src/context/WalletProvider/SelectModal.tsx
@@ -1,5 +1,5 @@
 import { Button, Center, Flex, ModalBody, ModalHeader, Stack, Tag } from '@chakra-ui/react'
-import { useTranslate } from 'react-polyglot'
+// import { useTranslate } from 'react-polyglot'
 import { RawText, Text } from 'components/Text'
 
 import { SUPPORTED_WALLETS } from './config'
@@ -10,7 +10,7 @@ export const SelectModal = () => {
     state: { adapters, walletInfo },
     connect
   } = useWallet()
-  const translate = useTranslate()
+  // const translate = useTranslate()
 
   return (
     <>
